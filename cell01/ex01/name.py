34b7f4e3bc7cff9c50737b4cecc71ec24a451b2a
wsenaset <warattaya.senaset@gmail.com>
2025-06-18 09:20:57 +0000

    hgdf

--- cell01/ex01/name.py
+++ cell01/ex01/name.py
@@ -1,4 +1,4 @@
-first_name = "first"
-last_name = "last"
+first_name = "warattaya"
+last_name = "senaset"
 whole_name = first_name + " " + last_name
 print(whole_name)
---
+++ cell04/ex01/age.py
@@ -0,0 +1,2 @@
+age = int(input("Please tell me your age: "))
+print(f"You are currently {age} years old.")
