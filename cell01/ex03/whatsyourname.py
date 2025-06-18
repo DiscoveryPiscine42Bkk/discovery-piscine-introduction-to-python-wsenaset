844802dc08b76e005e50b488ced614ac5c94517d
wsenaset <warattaya.senaset@gmail.com>
2025-06-18 10:02:47 +0000

    jvnkfj

--- cell01/ex03/whatsyourname.py
+++ cell01/ex03/whatsyourname.py
@@ -1,3 +1,3 @@
-first_name = input("Hey, what's your first name? : warattaya")
-last_name = input("And your last name? : senaset")
-print("Well, pleased to meet you,", first_name, last_name) 
+first_name = input("Hey, what's your first name? :warattaya ")
+last_name = input("And your last name? :senaset ")
+print("Well, pleased to meet you,", warattaya, senaset)
